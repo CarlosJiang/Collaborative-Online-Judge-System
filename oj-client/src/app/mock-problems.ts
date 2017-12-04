090155b7901e697de26a9688a6eb0accbc442d00
CarlosJiang <riverjiangjh@gmail.com>
2017-12-05 09:46:31 +1030

    Upload COJS

---
+++ oj-client/src/app/mock-problems.ts
@@ -0,0 +1,26 @@
+import { Problem } from './data-structure/problem'
+
+export const PROBLEMS: Problem[] = [
+  {
+    id:1,
+    name: "Two Sum",
+    desc: 'testing',
+    difficulty:"easy"
+  },
+
+  {
+    id:2,
+    name: "Two",
+    desc: 'testing',
+    difficulty:"medium"
+  },
+
+  {
+    id:3,
+    name: "Sum",
+    desc: 'testing',
+    difficulty:"hard"
+  }
+
+
+];
